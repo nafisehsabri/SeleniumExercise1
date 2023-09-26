cdaa99d9e36f94df066c0096ac8a726912ee7664
nafisehsabri <nafiseh.sabri@gmail.com>
2023-09-25 23:09:17 -0500

    selenium hw01

---
+++ src/test/java/HomeWorks/HomeWork01.java
@@ -0,0 +1,31 @@
+package HomeWorks;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeWork01 {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        WebElement userName= driver.findElement(By.name("username"));
+        userName.sendKeys("abc");
+
+        userName.sendKeys(Keys.CONTROL+"a");
+        userName.sendKeys(Keys.DELETE);
+        userName.sendKeys("Admin");
+        WebElement password= driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+
+        WebElement loginBtn=driver.findElement(By.tagName("button"));
+        System.out.println("The text: "+loginBtn.getText());
+        loginBtn.click();
+
+
+    }
+}
